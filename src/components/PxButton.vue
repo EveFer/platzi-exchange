54e56e0a921f48f9dd41b5d1eab0c356b6d532f8
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-02-28 17:47:28 -0600

    [ADD] Project kick off

---
+++ src/components/PxButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    @click="onClick"
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
+  >
+    <beat-loader :loading="isLoading" :color="'#68d391'" :size="8" />
+    <p v-show="!isLoading">
+      <slot></slot>
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PxButton',
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('custom-click')
+    }
+  }
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text gray-700 text-6xl">Platzi Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener las cotizaciones de las cryptomonedas más
+      importantes a traves de la API REST de coincap
+      <br />Este proyecto es utilizado en el curso de Vue.js Básico de Platxi
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
+      Volver a la pagina de Inicio
+    </router-link>
+  </div>
+</template>
